ef5489f80981398375a95f3cfe2a0a6c9f361fea
JohanSandager <jsbe@itu.dk>
2024-08-29 11:11:43 +0200

    Exercise 1.1

--- Intro2.fs
+++ Intro2.fs
@@ -6,44 +6,96 @@ module Intro2
 
 (* Association lists map object language variables to their values *)
 
-let env = [("a", 3); ("c", 78); ("baf", 666); ("b", 111)];;
+let max val1 val2 = if val1 > val2 then val1 else val2
+let min val1 val2 = if val1 < val2 then val1 else val2
+let eq val1 val2 = if val1 = val2 then 1 else 0
 
-let emptyenv = []; (* the empty environment *)
+
+let env = [ ("a", 3); ("c", 78); ("baf", 666); ("b", 111) ]
+
+let emptyenv = [] (* the empty environment *)
 
 let rec lookup env x =
-    match env with 
-    | []        -> failwith (x + " not found")
-    | (y, v)::r -> if x=y then v else lookup r x;;
+    match env with
+    | [] -> failwith (x + " not found")
+    | (y, v) :: r -> if x = y then v else lookup r x
 
-let cvalue = lookup env "c";;
+let cvalue = lookup env "c"
 
 
 (* Object language expressions with variables *)
 
-type expr = 
-  | CstI of int
-  | Var of string
-  | Prim of string * expr * expr;;
+type expr =
+    | CstI of int
+    | Var of string
+    | Prim of string * expr * expr
+    | If of expr * expr * expr
 
-let e1 = CstI 17;;
+let e1 = CstI 17
 
-let e2 = Prim("+", CstI 3, Var "a");;
+let e2 = Prim("+", CstI 3, Var "a")
 
-let e3 = Prim("+", Prim("*", Var "b", CstI 9), Var "a");;
+let e3 = Prim("+", Prim("*", Var "b", CstI 9), Var "a")
 
 
 (* Evaluation within an environment *)
 
-let rec eval e (env : (string * int) list) : int =
+let rec eval e (env: (string * int) list) : int =
     match e with
-    | CstI i            -> i
-    | Var x             -> lookup env x 
+    | CstI i -> i
+    | Var x -> lookup env x
     | Prim("+", e1, e2) -> eval e1 env + eval e2 env
     | Prim("*", e1, e2) -> eval e1 env * eval e2 env
     | Prim("-", e1, e2) -> eval e1 env - eval e2 env
-    | Prim _            -> failwith "unknown primitive";;
+    | Prim("max", e1, e2) -> max (eval e1 env) (eval e2 env)
+    | Prim("min", e1, e2) -> min (eval e1 env) (eval e2 env)
+    | Prim("==", e1, e2) -> eq (eval e1 env) (eval e2 env)
+    | Prim _ -> failwith "unknown primitive"
 
-let e1v  = eval e1 env;;
-let e2v1 = eval e2 env;;
-let e2v2 = eval e2 [("a", 314)];;
-let e3v  = eval e3 env;;
+let e1v = eval e1 env
+let e2v1 = eval e2 env
+let e2v2 = eval e2 [ ("a", 314) ]
+let e3v = eval e3 env
+
+
+// 1.1 ii
+eval (Prim("max", CstI 1, CstI 2)) []
+eval (Prim("min", CstI 2, CstI 1)) []
+eval (Prim("==", CstI 1, CstI 1)) []
+
+// 1.1 iii
+let rec eval2 e (env: (string * int) list) : int =
+    match e with
+    | CstI i -> i
+    | Var x -> lookup env x
+    | Prim(op, e1, e2) ->
+        let i1 = eval2 e1 env
+        let i2 = eval2 e2 env
+
+        match op with
+        | "+" -> i1 + i2
+        | "-" -> i1 - i2
+        | "*" -> i1 * i2
+        | "max" -> max i1 i2
+        | "min" -> min i1 i2
+        | "==" -> eq i1 i2
+    | Prim _ -> failwith "unknown primitive"
+
+// 1.1 iv && v
+let rec eval3 e (env: (string * int) list) : int =
+    match e with
+    | CstI i -> i
+    | Var x -> lookup env x
+    | Prim(op, e1, e2) ->
+        let i1 = eval2 e1 env
+        let i2 = eval2 e2 env
+
+        match op with
+        | "+" -> i1 + i2
+        | "-" -> i1 - i2
+        | "*" -> i1 * i2
+        | "max" -> max i1 i2
+        | "min" -> min i1 i2
+        | "==" -> eq i1 i2
+    | Prim _ -> failwith "unknown primitive"
+    | If(e1, e2, e3) -> if eval e1 env != 0 then eval e2 env else eval e3 env
