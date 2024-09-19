778778b2ac764581b5f998dc7a96daa4d7dc2d4d
JohanSandager <jsbe@itu.dk>
2024-09-19 11:54:26 +0200

    :P)

--- Fun/Parse.fs
+++ Fun/Parse.fs
@@ -5,31 +5,34 @@ module Parse
 open System
 open System.IO
 open System.Text
-open (*Microsoft.*)FSharp.Text.Lexing
+open FSharp.Text.Lexing
 open Absyn
 
 (* Plain parsing from a string, with poor error reporting *)
 
-let fromString (str : string) : expr =
-    let lexbuf = (*Lexing. insert if using old PowerPack *)LexBuffer<char>.FromString(str)
-    try 
-      FunPar.Main FunLex.Token lexbuf
-    with 
-      | exn -> let pos = lexbuf.EndPos 
-               failwithf "%s near line %d, column %d\n" 
-                  (exn.Message) (pos.Line+1) pos.Column
-             
+let fromString (str: string) : expr =
+    let lexbuf = (*Lexing. insert if using old PowerPack *)
+        LexBuffer<char>.FromString(str)
+
+    try
+        FunPar.Main FunLex.Token lexbuf
+    with exn ->
+        let pos = lexbuf.EndPos
+        failwithf "%s near line %d, column %d\n" (exn.Message) (pos.Line + 1) pos.Column
+
 (* Parsing from a file *)
 
-let fromFile (filename : string) =
+let fromFile (filename: string) =
     use reader = new StreamReader(filename)
-    let lexbuf = (* Lexing. insert if using old PowerPack *) LexBuffer<char>.FromTextReader reader
-    try 
-      FunPar.Main FunLex.Token lexbuf
-    with 
-      | exn -> let pos = lexbuf.EndPos 
-               failwithf "%s in file %s near line %d, column %d\n" 
-                  (exn.Message) filename (pos.Line+1) pos.Column
+
+    let lexbuf = (* Lexing. insert if using old PowerPack *)
+        LexBuffer<char>.FromTextReader reader
+
+    try
+        FunPar.Main FunLex.Token lexbuf
+    with exn ->
+        let pos = lexbuf.EndPos
+        failwithf "%s in file %s near line %d, column %d\n" (exn.Message) filename (pos.Line + 1) pos.Column
 
 (* Exercise it *)
 
@@ -38,25 +41,27 @@ let e2 = fromString "let f x = x + 7 in f 2 end"
 
 (* Examples in concrete syntax *)
 
-let ex1 = fromString 
-            @"let f1 x = x + 1 in f1 12 end"
+let ex1 = fromString @"let f1 x = x + 1 in f1 12 end"
 
 (* Example: factorial *)
 
-let ex2 = fromString 
-            @"let fac x = if x=0 then 1 else x * fac(x - 1)
+let ex2 =
+    fromString
+        @"let fac x = if x=0 then 1 else x * fac(x - 1)
               in fac n end"
 
 (* Example: deep recursion to check for constant-space tail recursion *)
 
-let ex3 = fromString 
-            @"let deep x = if x=0 then 1 else deep(x-1) 
+let ex3 =
+    fromString
+        @"let deep x = if x=0 then 1 else deep(x-1) 
               in deep count end"
-    
+
 (* Example: static scope (result 14) or dynamic scope (result 25) *)
 
-let ex4 = fromString 
-            @"let y = 11
+let ex4 =
+    fromString
+        @"let y = 11
               in let f x = x + y
                  in let y = 22 in f 3 end 
                  end
@@ -64,9 +69,18 @@ let ex4 = fromString
 
 (* Example: two function definitions: a comparison and Fibonacci *)
 
-let ex5 = fromString
-            @"let ge2 x = 1 < x
+let ex5 =
+    fromString
+        @"let ge2 x = 1 < x
               in let fib n = if ge2(n) then fib(n-1) + fib(n-2) else 1
                  in fib 25 
                  end
               end"
+
+//4.2
+let sum = "let sum x = if x=1 then x else sum(x+1) in sum count end"
+// eval (fromString "let sum x = if x=1 then x else x+ sum(x-1) in sum count end") [("count", Int 1000)];;
+// val it: int = 500500
+
+let 3eight = "let pow x = if x=0 then 1 else 3*pow(x-1) in pow exponent end"
+// eval (fromString "let pow x = if x=0 then 1 else 3*pow(x-1) in pow exponent end") [("exponent", Int 8)];;
